ab3de1226bbf79cd1b2b931984c80ff5615cb7d0
Isabel Bracho <127256795+ishaba1@users.noreply.github.com>
2023-11-04 17:26:53 -0300

    [cfg] -base uat

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <div>
       <h1>APP PARA PELICULAS</h1>
-      <h2>PRUEBA TEST ESTO FUNCIONA</h2>
+      <h2>ESTO ES PARA QUE EL CLIENTE PRUEBE</h2>
     </div>
   );
 };
