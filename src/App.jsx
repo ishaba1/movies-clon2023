9d4662e73390e173f069c94c76971814c8416900
Isabel Bracho <127256795+ishaba1@users.noreply.github.com>
2023-11-04 15:46:42 -0300

    [cfg] -esto funciona prueba test QA

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ const App = () => {
   return (
     <div>
       <h1>APP PARA PELICULAS</h1>
-      <h2>PRUEBA DEPLOY</h2>
+      <h2>PRUEBA TEST ESTO FUNCIONA</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
